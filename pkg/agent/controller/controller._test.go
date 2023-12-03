943839da57c14bac8cd5f503482966c260157292
samasno <emmanuel@dv-analytics.io>
2023-12-02 21:51:01 -0600

    scaffold initial tests

---
+++ pkg/agent/controller/controller._test.go
@@ -0,0 +1,23 @@
+package controller
+
+import "testing"
+
+func TestNewController(t *testing.T) {
+
+}
+
+func TestInit(t *testing.T) {
+
+}
+
+func TestRun(t *testing.T) {
+
+}
+
+func TestShutdown(t *testing.T) {
+
+}
+
+func TestStatus(t *testing.T) {
+
+}
