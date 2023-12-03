86b5c4232d4583519ec40aa4e8178c39411fa97a
samasno <emmanuel@dv-analytics.io>
2023-12-02 21:49:38 -0600

    add Close to Logger

--- pkg/agent/types/types.go
+++ pkg/agent/types/types.go
@@ -1,16 +1,42 @@
-package t
+package types
 
 import (
 	"crypto/tls"
 	"net"
 )
 
-type ControllerInitInput struct {
-	UpstreamDetails Upstream
-	LogPaths        []string
+type Controller interface { // manages the forwarder to upstream and log runners
+	Init() error
+	Run() error
+	Shutdown() error
+	Status() (Status, error)
 }
 
-type ControllerStatus struct{}
+type Forwarder interface {
+	Connect() error
+	Close() error
+	Push([]byte) error
+	Status() (Status, error)
+}
+
+type LogTail interface {
+	Open() error
+	Close() error
+	Update() error
+	Status() (Status, error)
+}
+
+type Logger interface {
+	StdOut() error
+	StdErr() error
+	Close() error
+	Status() (Status, error)
+}
+
+type Status interface {
+	Message() string
+	Ok() bool
+}
 
 type Upstream struct {
 	Connection net.Conn
