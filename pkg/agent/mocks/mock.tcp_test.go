ed6a800211faabb8e39e8a3aaa4564694e1b3fec
samasno <emmanuel@dv-analytics.io>
2023-12-08 14:12:01 -0600

    tls server tests

--- pkg/agent/mocks/mock.tcp_test.go
+++ pkg/agent/mocks/mock.tcp_test.go
@@ -1,6 +1,9 @@
 package mock
 
 import (
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"net"
 	"sync"
 	"testing"
@@ -21,7 +24,6 @@ func TestTcpServer(t *testing.T) {
 	client1.Write([]byte("message 1 from test."))
 	client1.Write([]byte("message 2 from test."))
 	client1.Close()
-
 	wg.Add(1)
 	client2, err := net.Dial("tcp", addr)
 	if err != nil {
@@ -30,9 +32,35 @@ func TestTcpServer(t *testing.T) {
 	client2.Write([]byte("message 3 from test."))
 	client2.Write([]byte("message 4 from test."))
 	client2.Close()
-
 	err = closer()
 	if err != nil {
+		t.Fatal(err.Error())
+	}
+}
+
+func TestTlsTcpServer(t *testing.T) {
+	wg := &sync.WaitGroup{}
+	closer := TlsTcpServer(":8080", wg)
+	if closer == nil {
+		t.Fatal("Failed to start server")
+	}
+	cert, err := ioutil.ReadFile("../../../certs/testing.crt")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(cert)
+	tlsConfig := &tls.Config{RootCAs: caCertPool, ServerName: "localhost", InsecureSkipVerify: true}
+	client, err := tls.Dial("tcp", ":8080", tlsConfig)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	wg.Add(1)
+	client.Write([]byte("tls test message"))
+	client.Close()
+	err = closer()
+	if err != nil {
+		println("Failed to stop server")
 		t.Fatal(err.Error())
 	}
 }
