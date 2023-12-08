ffc899050fe187d8f0c0fb9894dad08c44d0ae24
samasno <emmanuel@dv-analytics.io>
2023-12-08 13:02:18 -0600

    vanilla server works

--- pkg/agent/mocks/mock.tcp.go
+++ pkg/agent/mocks/mock.tcp.go
@@ -1,19 +1,24 @@
 package mock
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io"
 	"net"
+	"strconv"
 	"sync"
 )
 
 var connId = 0
+var tlsConnId = 0
 
 func VanillaTcpServer(addr string, wg *sync.WaitGroup) func() error {
+	println("Starting mock tcp server at", addr)
 	ln, err := net.Listen("tcp", addr)
 	if err != nil {
 		return nil
 	}
+	println("Mock tcp server running at", addr)
 	go func(ln net.Listener) {
 		for {
 			conn, err := ln.Accept()
@@ -39,7 +44,7 @@ func VanillaTcpServer(addr string, wg *sync.WaitGroup) func() error {
 					}
 					if n > 0 {
 						fmt.Printf("Read %d bytes from connection %d\n", n, id)
-						println(string(b))
+						println(string(b[:n]))
 					}
 				}
 				println("closing handle connection for id", id)
@@ -48,6 +53,52 @@ func VanillaTcpServer(addr string, wg *sync.WaitGroup) func() error {
 	}(ln)
 	return func() error {
 		wg.Wait()
+		return ln.Close()
+	}
+}
+
+func TlsTcpServer(addr string, wg *sync.WaitGroup) func() error {
+	config := &tls.Config{}
+	ln, err := tls.Listen("tcp", addr, config)
+	if err != nil {
+		return nil
+	}
+	go func(ln net.Listener, wg *sync.WaitGroup) {
+		println("mock tls server listening at " + addr)
+		for {
+			conn, err := ln.Accept()
+			if err != nil {
+				println("Server closed, shutting down")
+				break
+			}
+			tlsConnId++
+			go func(conn net.Conn, id int) {
+				defer conn.Close()
+				b := make([]byte, 1024)
+				for {
+					n, err := conn.Read(b)
+					if err == io.EOF {
+						println("Reached end of stream tls id: " + strconv.Itoa(id))
+						break
+					}
+					if err != nil {
+						println(err.Error())
+						break
+					}
+					if n > 0 {
+						fmt.Printf("Read %d bytes from connection %d\n", n, id)
+						println(string(b[:n]))
+					}
+				}
+				println("Closing connection handler for " + strconv.Itoa(id))
+			}(conn, tlsConnId)
+		}
+	}(ln, wg)
+	return func() error {
+		println("Draining connections")
+		wg.Wait()
+		println("No connections remaining")
+		println("Closing listener")
 		return ln.Close()
 	}
 }
