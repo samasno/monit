3e8354eb3de19ddfdc0e1e088fb8755b12857a75
samasno <emmanuel@dv-analytics.io>
2023-12-26 12:47:40 -0600

    basic socket emitter

---
+++ pkg/agent/emitters/emitters.go
@@ -0,0 +1,53 @@
+package emitters
+
+import (
+	"encoding/json"
+	"net"
+
+	"github.com/samasno/monit/pkg/agent/types"
+)
+
+type SocketEmitter struct {
+	conn  *net.UnixConn
+	Raddr string
+	Laddr string
+}
+
+func (s *SocketEmitter) Emit(event types.Event) error {
+	if s.conn == nil {
+		err := s.connect()
+		if err != nil {
+			s.conn = nil
+			return err
+		}
+	}
+	e, err := json.Marshal(event)
+	if err != nil {
+		return err
+	}
+	_, err = s.conn.Write(e)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *SocketEmitter) connect() error {
+	if s.conn != nil {
+		return nil
+	}
+	laddr, err := net.ResolveUnixAddr("unixgram", s.Laddr)
+	if err != nil {
+		return err
+	}
+	raddr, err := net.ResolveUnixAddr("unixgram", s.Raddr)
+	if err != nil {
+		return err
+	}
+	cn, err := net.DialUnix("unixgram", laddr, raddr)
+	if err != nil {
+		return err
+	}
+	s.conn = cn
+	return nil
+}
